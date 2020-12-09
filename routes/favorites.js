84e5aaa60f9f43c16f85a7ea12202f75b8219da5
Alchemic <vitalij0202@gmail.com>
2020-12-10 00:19:05 +0300

    Added user authentication

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,72 @@
 const express = require('express');
 const router = express.Router();
+const Datastore = require('nedb')
+
+const favDb = new Datastore({ filename: './favorites.db', autoload: true });
+const usersDb = new Datastore( { filename: './users.db', autoload: true });
 
 router.get('/', (req, res, next) => {
+    const userId = req.cookies.userId;
+    if (!userId) {
+        return res.status(200).json([])
+    }
+    favDb.find({}, (error, docs) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log(docs);
+        }
+    });
+    res.cookie('userId', userId, { maxAge: 3600000 * 24 * 180 })
     res.status(200).json({
-        message: "Get favorites"
+        message: "Get favorites for user " + userId
     });
 });
+
+router.post('/', async (req, res, next) => {
+    let userId
+    if (req.cookies.userId) {
+        userId = req.cookies.userId;
+        if (!await validateId(userId)) {
+            userId = await getId();
+        }
+    }
+    else {
+        userId = await getId();
+        console.log(userId);
+    }
+    res.cookie('userId', userId, { maxAge: 3600000 * 24 * 180 })
+    console.log(req.body.name);
+    res.status(201).json({name: req.body.name});
+})
+
+function validateId(id) {
+    return new Promise((resolve, reject) => {
+        usersDb.find({ _id: id }, (err, docs) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                resolve(docs.length !== 0);
+            }
+        })
+    })
+}
+
+function getId() {
+    return new Promise((resolve, reject) => {
+        usersDb.insert({}, (err, doc) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                resolve(doc._id);
+            }
+        })
+    });
+}
 
 module.exports = router;
--- routes/weather.js
+++ routes/weather.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch')
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Get weather"
-    });
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric'
+const appid = '3a8e27db2f53d5233b5e559948a133b6'
+
+router.get('/city', (req, res, next) => {
+    const city = req.query.q;
+    const lang = req.query.lang;
+    fetch(`${baseUrl}&appid=${appid}&q=${city}&lang=${lang}`)
+        .then(r => r.json())
+        .then(r => {
+            console.log(r);
+            res.status(200).json(r);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.get('/coordinates', (req, res, next) => {
+    const lat = req.query.lat;
+    const long = req.query.long;
+    const lang = req.query.lang;
+    fetch(`${baseUrl}&appid=${appid}&lat=${lat}&lon=${long}&lang=${lang}`)
+        .then(r => r.json())
+        .then(r => {
+            console.log(r);
+            res.status(200).json(r);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
